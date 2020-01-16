d5cb3d73e19fced1be59d9470d1579c4c092daef
Arthur Dubedat <arthur.dubedat@gmail.com>
2020-01-16 18:03:33 +0100

    [fix] Add json and urlencoded middleware
    json and urlencoded were not handled in the request body

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 // const routes = require('./src/server/routes');
+import bodyParser from 'body-parser';
 import routes from './src/server/routes';
 import params from './params';
 
@@ -9,6 +10,8 @@ const port = process.env.PORT || params.port;
 const app = express();
 
 app.listen(port);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 routes(app);
 
 console.log(`Music Room RESTful API server started on port: ${port}`);
