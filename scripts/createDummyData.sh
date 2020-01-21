914f6a068d66b84d684765cc08672126b4b50ecc
Arthur Dubedat <arthur.dubedat@gmail.com>
2020-01-21 13:54:15 +0100

    [fix] createDummyData script now create username as strings
    The script inserted usename as integer

--- scripts/createDummyData.sh
+++ scripts/createDummyData.sh
@@ -1,22 +1,22 @@
 for i in 1 2 3 4 5 6 7
 do
-echo "INSERT INTO UserProfiles (Username, Email, CreatedAt) VALUES ("$i", '"$i@$i.$i"', '2020-01-17 15:15:15');"
+echo "INSERT INTO UserProfiles (Username, Email, CreatedAt) VALUES ('"$i"', '"$i@$i.$i"', '2020-01-17 15:15:15');"
 
 if [ $i -lt 5 ]
 then
 echo "INSERT INTO UserAccounts (UserProfileId, Email, EmailConfirmationPin) "
-echo "SELECT id, email, $i FROM UserProfiles WHERE Username = "$i";"
+echo "SELECT id, email, $i FROM UserProfiles WHERE Username = '"$i"';"
 fi
 
 if [ $i = 1 ] || [ $i = 2 ] || [ $i = 5 ] || [ $i = 7 ]
 then
 echo "INSERT INTO ProviderAccounts (UserProfileId, Provider, ProviderId) "
-echo "SELECT id, 'Google', $i FROM UserProfiles WHERE Username = "$i";"
+echo "SELECT id, 'Google', $i FROM UserProfiles WHERE Username = '"$i"';"
 fi
 
 if [ $i = 2 ] || [ $i = 4 ] || [ $i = 6 ] || [ $i = 7 ]
 then
 echo "INSERT INTO ProviderAccounts (UserProfileId, Provider, ProviderId) "
-echo "SELECT id, 'Facebook', $i FROM UserProfiles WHERE Username = "$i";"
+echo "SELECT id, 'Facebook', $i FROM UserProfiles WHERE Username = '"$i"';"
 fi
 done
