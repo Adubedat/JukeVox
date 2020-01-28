ba272bed61d6da32867b7ae32675ffc88699d627
hadleycode <hadleydaniel@gmail.com>
2020-01-28 17:12:55 +0100

    [test] Add tests for POST /users and GET /users/:email/accounts

--- test/userTests.js
+++ test/userTests.js
@@ -35,4 +35,68 @@ describe('Users', () => {
         });
     });
   });
+
+  /*
+  * Test the /POST route
+  */
+
+  describe('/POST user', () => {
+    it('should not POST a user without a password field', (done) => {
+      const user = {
+        username: 'Daniel',
+        email: 'daniel@mail.com',
+      };
+      chai.request(server)
+        .post('/users')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('status').eql('error');
+          res.body.should.have.property('statusCode');
+          res.body.should.have.property('message');
+          done();
+        });
+    });
+
+    it('should POST a user', (done) => {
+      const user = {
+        username: 'Daniel',
+        email: 'daniel@mail.com',
+        password: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      };
+      chai.request(server)
+        .post('/users')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('User successfully added!');
+          done();
+        });
+    });
+  });
+
+  /*
+  * Test the /GET/users/:email/accounts route
+  */
+  describe('/GET/users/:email/accounts', () => {
+    it('should GET a user accounts by a given email address', (done) => {
+      const user = {
+        username: 'Daniel',
+        email: 'daniel@mail.com',
+        password: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+      };
+      // TODO add user to db and in callback
+      chai.request(server)
+        .get(`/users/${user.email}/accounts`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('data');
+          // TODO: Add tests to check just UserAccount is returned
+          done();
+        });
+    });
+  });
 });
