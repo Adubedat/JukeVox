1aba97c20bc67ead33f73bfc798183e1d994c3f8
Arthur Dubedat <arthur.dubedat@gmail.com>
2020-01-29 17:44:52 +0100

    [style] Add return before resolve for linter

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -14,7 +14,7 @@ class Database {
     return new Promise((resolve, reject) => {
       this.connection.query(sql, args, (err, rows) => {
         if (err) { return reject(err); }
-        resolve(rows);
+        return resolve(rows);
       });
     });
   }
@@ -23,7 +23,7 @@ class Database {
     return new Promise((resolve, reject) => {
       this.connection.end((err) => {
         if (err) { return reject(err); }
-        resolve();
+        return resolve();
       });
     });
   }
