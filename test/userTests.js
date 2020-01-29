8fedf52fc9c0e96abe295ad80ee15b5bf6d67c8f
hadleycode <hadleydaniel@gmail.com>
2020-01-29 14:43:52 +0100

    [test] [WIP] Start building more solid tests for GET /users
    Up until now just testing one case, added lots of todos to cover
    many more of the cases

--- test/userTests.js
+++ test/userTests.js
@@ -2,6 +2,8 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import sql from '../db';
+import moment from 'moment';
+import DATETIME_FORMAT from '../src/server/constants';
 
 
 const chai = require('chai');
@@ -15,8 +17,6 @@ chai.use(chaiHttp);
 
 describe('Users', () => {
   beforeEach((done) => {
-    // TODO: Before each test, clear the DB
-
     let query = 'DELETE FROM UserAccounts;';
     sql.query(query, (err, res) => {
       if (err) {
@@ -46,16 +46,57 @@ describe('Users', () => {
   */
 
   describe('/GET users', () => {
-    it('should GET all the users', (done) => {
+    it('should not GET all the users if the database is empty', (done) => {
       chai.request(server)
-        .get('/users/search')
+        .get('/users')
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.status(404);
           res.body.should.be.a('object');
+          res.body.should.have.property('status').eql('error');
+          res.body.should.have.property('statusCode');
           res.body.should.have.property('message');
-          res.body.should.have.property('data');
           done();
         });
+    });
+
+
+    it('should GET all the users', (done) => {
+      const query = 'INSERT INTO UserProfiles (Username, Email, CreatedAt) VALUES ?'
+      const values = [['Daniel', 'daniel@mail.com', moment().format(DATETIME_FORMAT)]];
+      sql.query(query, [values], (err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          chai.request(server)
+          .get('/users')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message');
+            res.body.should.have.property('data');
+            res.body.data.should.be.a('array')
+            res.body.data.length.should.be.eql(1);
+            res.body.data[0].should.have.property('Id');
+            res.body.data[0].should.have.property('Username');
+            res.body.data[0].should.have.property('Email');
+            res.body.data[0].should.have.property('ProfilePicture');
+            console.log(res.body.data[0]);
+            res.body.data[0].should.have.all.keys('Id', 'Username', 'Email', 'ProfilePicture');
+            done();
+          });
+        }
+      })
+
+      //TODO: GET /users with a username as query
+      //TODO: GET /users with a wrong username as query
+      //TODO: GET /users with an email address as query
+      //TODO: GET /users with a wrong email address as query
+      //TODO: GET /users with BOTH a username and email address as query
+      //TODO: GET /users with BOTH a wrong username and wrong email address as query
+      //TODO: GET /users with BOTH a right username and wrong email address as query
+      //TODO: GET /users with BOTH a wrong username and right email address as query
+      //TODO: GET /users with a non existant query
+
     });
   });
 
@@ -122,4 +163,6 @@ describe('Users', () => {
         });
     });
   });
+
+
 });
