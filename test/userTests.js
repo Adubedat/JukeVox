001f67b06accad15471ba3396b4efa8f38f9735a
hadleycode <hadleydaniel@gmail.com>
2020-02-03 16:06:26 +0100

    [test] Add tests for POST /users
    Add tests for checking if
    no username, no password, no email
    invalid username, invalid password, invalid email
    existing username, existing email

--- test/userTests.js
+++ test/userTests.js
@@ -237,7 +237,6 @@ describe('Users', () => {
       res.body.should.have.property('status').eql('error');
       res.body.should.have.property('statusCode');
       res.body.should.have.property('message');
-      res.body.message.should.eql('No user found');
     });
   });
 
@@ -255,14 +254,156 @@ describe('Users', () => {
         .post('/users')
         .send(user)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.status(400);
           res.body.should.be.a('object');
           res.body.should.have.property('status').eql('error');
           res.body.should.have.property('statusCode');
           res.body.should.have.property('message');
+          res.body.message.should.eql('No password was supplied')
           done();
         });
     });
+
+      it('should not POST a user without a username field', (done) => {
+        const user = {
+          email: 'daniel@mail.com',
+          password: 'abcdefghijk',
+        };
+        chai.request(server)
+          .post('/users')
+          .send(user)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('status').eql('error');
+            res.body.should.have.property('statusCode');
+            res.body.should.have.property('message');
+            res.body.message.should.eql('No username was supplied')
+            done();
+          });
+      });
+
+        it('should not POST a user without an email field', (done) => {
+          const user = {
+            username: 'Daniel',
+            password: 'abcdefghijk',
+          };
+          chai.request(server)
+            .post('/users')
+            .send(user)
+            .end((err, res) => {
+              res.should.have.status(400);
+              res.body.should.be.a('object');
+              res.body.should.have.property('status').eql('error');
+              res.body.should.have.property('statusCode');
+              res.body.should.have.property('message');
+              res.body.message.should.eql('No email was supplied')
+              done();
+            });
+        });
+
+          it('should not POST a user with an invalid username field', (done) => {
+            const user = {
+              username: 'Daniel&Daniel',
+              password: 'abcdefghijk',
+              email: 'daniel@mail.com',
+            };
+            chai.request(server)
+              .post('/users')
+              .send(user)
+              .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('error');
+                res.body.should.have.property('statusCode');
+                res.body.should.have.property('message');
+                res.body.message.should.eql('Username is invalid')
+                done();
+              });
+          });
+
+          it('should not POST a user with an invalid password field', (done) => {
+            const user = {
+              username: 'Daniel',
+              password: 'abc',
+              email: 'daniel@mail.com',
+            };
+            chai.request(server)
+              .post('/users')
+              .send(user)
+              .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('error');
+                res.body.should.have.property('statusCode');
+                res.body.should.have.property('message');
+                res.body.message.should.eql('Password is too short')
+                done();
+              });
+          });
+
+          it('should not POST a user with an invalid email field', (done) => {
+            const user = {
+              username: 'Daniel',
+              password: 'abcdefghijk',
+              email: 'daniel',
+            };
+            chai.request(server)
+              .post('/users')
+              .send(user)
+              .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eql('error');
+                res.body.should.have.property('statusCode');
+                res.body.should.have.property('message');
+                res.body.message.should.eql('Email is invalid')
+                done();
+              });
+          });
+
+          it('should not POST a user with an existing username', async () => {
+
+            const query = 'INSERT INTO UserProfiles (Username, Email, CreatedAt) VALUES ?'
+            const values = [['Daniel', 'daniel@mail.com', moment().format(DATETIME_FORMAT)]];
+            await sql.query(query, [values]).catch((err) => console.log(err));
+
+            const user = {
+              username: 'Daniel',
+              password: 'abcdefghijk',
+              email: 'daniel2@mail.com',
+            };
+
+            const res = await chai.request(server).post('/users').send(user);
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('status').eql('error');
+            res.body.should.have.property('statusCode');
+            res.body.should.have.property('message');
+            res.body.message.should.eql('Username already exists')
+          });
+
+          //TODO: Add two extra test to see if existing email still passes when email exists in useraccounts or provideraccounts
+          it('should not POST a user with an existing email', async () => {
+
+            const query = 'INSERT INTO UserProfiles (Username, Email, CreatedAt) VALUES ?'
+            const values = [['Daniel', 'daniel@mail.com', moment().format(DATETIME_FORMAT)]];
+            await sql.query(query, [values]).catch((err) => console.log(err));
+
+            const user = {
+              username: 'Daniel2',
+              password: 'abcdefghijk',
+              email: 'daniel@mail.com',
+            };
+
+            const res = await chai.request(server).post('/users').send(user);
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('status').eql('error');
+            res.body.should.have.property('statusCode');
+            res.body.should.have.property('message');
+            res.body.message.should.eql('Email already exists')
+          });
 
     it('should POST a user', (done) => {
       const user = {
