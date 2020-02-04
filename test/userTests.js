5ab0cfc3d57cfa23ac4687631e15615d16f0d51f
hadleycode <hadleydaniel@gmail.com>
2020-02-04 18:17:25 +0100

    [test] Finish tests for GET /users/:email/accounts
    Tested for:
    - user account
    - deezer
    - facebook
    - google
    - all 3 providers
    - classic + providers
    - invalid email
    - non existant email

--- test/userTests.js
+++ test/userTests.js
@@ -449,7 +449,17 @@ describe('Users', () => {
       return userAccount;
     }
 
-    it('should GET a user account by a given email address', async () => {  
+    async function addProviderAccount(type, id) {
+
+      const providerAccountQuery = 'INSERT INTO ProviderAccounts (UserProfileId, Provider, ProviderId) \
+      VALUES ?';
+      const providerAccountValues = [[id, type, 'a']]
+      const providerAccount = await sql.query(providerAccountQuery, [providerAccountValues]).catch((err) => console.log(err));
+
+      return providerAccount;
+    }
+
+    it('should GET a only a classic user account by a given email address', async () => {  
       const email = 'daniel@mail.com';    
       const userProfile = await addUserProfile(email);
       await addUserAccount(userProfile.insertId, email);
@@ -464,13 +474,153 @@ describe('Users', () => {
       res.body.accountTypes[0].should.be.eql('Classic');
     });
 
-    // TODO: user has only provider google
-    // TODO: user has only provider facebook
-    // TODO: user has useraccount + providers
-    // TODO: user email invalid 
-    // TODO: user email inexistant 
+    it('should GET a only a google user account by a given email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Google', userProfile.insertId);
 
+      const res = await chai.request(server).get(`/users/${email}/accounts`);
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('Email matches these account types');  
+      res.body.should.have.property('accountTypes');
+      res.body.accountTypes.should.be.a('array');
+      res.body.accountTypes.length.should.be.eql(1);
+      res.body.accountTypes[0].should.be.eql('Google');
+    });
 
+    it('should GET a only a google user account by a given email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Google', userProfile.insertId);
+
+      const res = await chai.request(server).get(`/users/${email}/accounts`);
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('Email matches these account types');  
+      res.body.should.have.property('accountTypes');
+      res.body.accountTypes.should.be.a('array');
+      res.body.accountTypes.length.should.be.eql(1);
+      res.body.accountTypes[0].should.be.eql('Google');
+    });
+
+    it('should GET a only a facebook user account by a given email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Facebook', userProfile.insertId);
+
+      const res = await chai.request(server).get(`/users/${email}/accounts`);
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('Email matches these account types');  
+      res.body.should.have.property('accountTypes');
+      res.body.accountTypes.should.be.a('array');
+      res.body.accountTypes.length.should.be.eql(1);
+      res.body.accountTypes[0].should.be.eql('Facebook');
+    });
+
+    it('should GET a only a deezer user account by a given email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Deezer', userProfile.insertId);
+
+      const res = await chai.request(server).get(`/users/${email}/accounts`);
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('Email matches these account types');  
+      res.body.should.have.property('accountTypes');
+      res.body.accountTypes.should.be.a('array');
+      res.body.accountTypes.length.should.be.eql(1);
+      res.body.accountTypes[0].should.be.eql('Deezer');
+    });
+
+    it('should GET a deezer + google + facebook user account by a given email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Deezer', userProfile.insertId);
+      await addProviderAccount('Google', userProfile.insertId);
+      await addProviderAccount('Facebook', userProfile.insertId);
+
+      const res = await chai.request(server).get(`/users/${email}/accounts`);
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('Email matches these account types');  
+      res.body.should.have.property('accountTypes');
+      res.body.accountTypes.should.be.a('array');
+      res.body.accountTypes.length.should.be.eql(3);
+      res.body.accountTypes[2].should.be.eql('Deezer');
+      res.body.accountTypes[1].should.be.eql('Google');
+      res.body.accountTypes[0].should.be.eql('Facebook');
+    });
+
+    it('should GET a classic and google user account by a given email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Google', userProfile.insertId);
+      await addUserAccount(userProfile.insertId, email);
+
+      const res = await chai.request(server).get(`/users/${email}/accounts`);
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('Email matches these account types');  
+      res.body.should.have.property('accountTypes');
+      res.body.accountTypes.should.be.a('array');
+      res.body.accountTypes.length.should.be.eql(2);
+      res.body.accountTypes[0].should.be.eql('Classic');
+      res.body.accountTypes[1].should.be.eql('Google');
+    });
+
+    it('should GET a classic and google + deezer user account by a given email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Google', userProfile.insertId);
+      await addProviderAccount('Deezer', userProfile.insertId);
+      await addUserAccount(userProfile.insertId, email);
+
+      const res = await chai.request(server).get(`/users/${email}/accounts`);
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('Email matches these account types');  
+      res.body.should.have.property('accountTypes');
+      res.body.accountTypes.should.be.a('array');
+      res.body.accountTypes.length.should.be.eql(3);
+      res.body.accountTypes[0].should.be.eql('Classic');
+      res.body.accountTypes[1].should.be.eql('Google');
+      res.body.accountTypes[2].should.be.eql('Deezer');
+    });
+
+    it('should not GET a user account with an invalid email address', async () => {  
+      const email = 'daniel@mail.com';    
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Google', userProfile.insertId);
+      await addUserAccount(userProfile.insertId, email);
+
+      const res = await chai.request(server).get(`/users/daniel/accounts`);
+
+      res.should.have.status(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('Email invalid')
+    });
+
+    it('should not GET a user account with a non existant email address', async () => {  
+      const email = 'daniel@mail.com';
+      const unknown = 'unknown@mail.com';
+      const userProfile = await addUserProfile(email);
+      await addProviderAccount('Google', userProfile.insertId);
+      await addUserAccount(userProfile.insertId, email);
+
+      const res = await chai.request(server).get(`/users/${unknown}/accounts`);
+
+      res.should.have.status(404);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('No account found for this email')
+    });
 
   });
 
