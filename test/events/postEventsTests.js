698df14403302d439f5fb6864a5e7ff91fdc3913
hadleycode <hadleydaniel@gmail.com>
2020-02-05 21:19:17 +0100

    [test] Add tests for POST /events
    COMPLETED: should not POST an event with a jwt that matches no user
    COMPLETED: should not POST an event without a jwt
    COMPLETED: should not POST an event with an invalid jwt

--- test/events/postEventsTests.js
+++ test/events/postEventsTests.js
@@ -60,6 +60,117 @@ describe('Events', () => {
       res.body.should.be.a('object');
       res.body.should.have.property('statusCode');
       res.body.should.have.property('message');
+      res.body.message.should.be.eql(body);
+
+      const createdEvents = await sql.query('SELECT * FROM Events');
+      createdEvents.should.have.lengthOf(1);
+      createdEvents[0].CreatorId.should.eql(user.insertId);
+      // TODO (?): check the rest of the elements are eql
     });
+
+    it('should not POST an event with a jwt that matches no user', async () => {
+      const body = {
+        name: 'House warming',
+        description: 'All come over on wednesday for our housewarming!',
+        startDate,
+        endDate,
+        latitude: 48.8915482,
+        longitude: 2.3170656,
+        streamerDevice: 'abcd',
+        isPrivate: true,
+      };
+
+      const jwt = generateJwt(-1);
+
+      const res = await chai.request(server)
+        .post('/api/events')
+        .set({ Authorization: `Bearer ${jwt}` })
+        .send(body);
+
+      res.should.have.status(404);
+      res.body.should.be.a('object');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('No account found: Wrong token provided');
+      const createdEvents = await sql.query('SELECT * FROM Events');
+      createdEvents.should.have.lengthOf(0);
+    });
+
+    it('should not POST an event without a jwt', async () => {
+      const body = {
+        name: 'House warming',
+        description: 'All come over on wednesday for our housewarming!',
+        startDate,
+        endDate,
+        latitude: 48.8915482,
+        longitude: 2.3170656,
+        streamerDevice: 'abcd',
+        isPrivate: true,
+      };
+
+      await addUserProfile();
+
+      const res = await chai.request(server)
+        .post('/api/events')
+        .send(body);
+
+      res.should.have.status(404);
+      res.body.should.be.a('object');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('Wrong token provided');
+      const createdEvents = await sql.query('SELECT * FROM Events');
+      createdEvents.should.have.lengthOf(0);
+    });
+
+
+    it('should not POST an event with an invalid jwt', async () => {
+      const body = {
+        name: 'House warming',
+        description: 'All come over on wednesday for our housewarming!',
+        startDate,
+        endDate,
+        latitude: 48.8915482,
+        longitude: 2.3170656,
+        streamerDevice: 'abcd',
+        isPrivate: true,
+      };
+
+      const user = await addUserProfile();
+      let jwt = generateJwt(user.insertId);
+      jwt = jwt.substring(0, jwt.length - 1);
+
+      const res = await chai.request(server)
+        .post('/api/events')
+        .set({ Authorization: `Bearer ${jwt}` })
+        .send(body);
+
+      res.should.have.status(404);
+      res.body.should.be.a('object');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('Wrong token provided');
+      const createdEvents = await sql.query('SELECT * FROM Events');
+      createdEvents.should.have.lengthOf(0);
+    });
+
+    // COMPLETED: should not POST an event with a jwt that matches no user
+    // COMPLETED: should not POST an event without a jwt
+    // COMPLETED: should not POST an event with an invalid jwt
+    // TODO: should not POST an event with an unknown field
+    // TODO: should not POST an event with name too long
+    // TODO: should not POST an event with name = null
+    // TODO: should not POST an event with description too long
+    // TODO: should not POST an event with start date in the past
+    // TODO: should not POST an event with end date before start date
+    // TODO: should not POST an event with no start date
+    // TODO: should not POST an event with no end date
+    // TODO: should not POST an event with unknown latitude
+    // TODO: should not POST an event with unknown longitude
+    // TODO: should not POST an event with no latitude
+    // TODO: should not POST an event with no longitude
+    // TODO: should not POST an event with no streamerDevice
+    // TODO: should not POST an event with no private property
+    // TODO: Discuss with others if there should be a min / max lenght for an event
   });
 });
