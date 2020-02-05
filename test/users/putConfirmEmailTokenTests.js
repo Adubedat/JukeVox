792543aa22e9cfc07dc27ee61cb45c8e02e58fcf
Arthur Dubedat <arthur.dubedat@gmail.com>
2020-02-05 19:12:35 +0100

    [test] confirmEmail error test

--- test/users/putConfirmEmailTokenTests.js
+++ test/users/putConfirmEmailTokenTests.js
@@ -39,5 +39,22 @@ describe('Users', () => {
       userAccount2.EmailConfirmed.should.eql(1);
       expect(userAccount2.EmailConfirmationString).to.equal(null);
     });
+    it('should not activate account with invalid token', async () => {
+      const user1 = await sql.query('INSERT INTO UserProfiles (Username, Email, CreatedAt) VALUES (\'user1\', \'test@test,test\', \'2020-12-12 12:12:12\')');
+      await sql.query(`INSERT INTO UserAccounts (UserProfileId, Email, EmailConfirmationString) VALUES (${user1.insertId}, 'test@test.test', 'confirmationString')`);
+      const [userAccount] = await sql.query('SELECT * FROM UserAccounts');
+      userAccount.EmailConfirmed.should.eql(0);
+      userAccount.EmailConfirmationString.should.eql('confirmationString');
+      const res = await chai.request(server)
+        .put('/confirmEmail/invalidConfirmationString');
+      res.should.have.status(404);
+      res.body.should.be.a('object');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('Token does not exist');
+      const [userAccount2] = await sql.query('SELECT * FROM UserAccounts');
+      userAccount2.EmailConfirmed.should.eql(0);
+      expect(userAccount2.EmailConfirmationString).to.equal('confirmationString');
+    });
   });
 });
