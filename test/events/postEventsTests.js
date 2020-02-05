0496eaff4706ca23d50124d95ee683d5e6b4b8cf
hadleycode <hadleydaniel@gmail.com>
2020-02-05 20:38:56 +0100

    [test] Add the first test for POST /events

---
+++ test/events/postEventsTests.js
@@ -0,0 +1,65 @@
+/* eslint-env node, mocha */
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import moment from 'moment';
+import DATETIME_FORMAT from '../../src/server/constants';
+import Database from '../../src/helpers/database';
+
+import { generateJwt } from '../../src/server/controller/userController';
+
+const sql = new Database();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Events', () => {
+  beforeEach(async () => {
+    await sql.query('DELETE FROM UserAccounts;');
+    await sql.query('DELETE FROM ProviderAccounts;');
+    await sql.query('DELETE FROM UserProfiles;');
+    await sql.query('DELETE FROM Events;');
+  });
+
+  async function addUserProfile() {
+    const userProfileQuery = 'INSERT INTO UserProfiles (Username, Email, CreatedAt) VALUES ?';
+    const userProfileValues = [['Daniel', 'daniel@mail.com', moment().format(DATETIME_FORMAT)]];
+    const userProfile = await sql.query(userProfileQuery, [userProfileValues]).catch((err) => console.log(err));
+    return userProfile;
+  }
+
+
+  describe('/POST /events', () => {
+    const startDate = moment().add(3, 'd').format(DATETIME_FORMAT);
+    const endDate = moment().add(4, 'd').format(DATETIME_FORMAT);
+
+    it('should POST an event', async () => {
+      const body = {
+        name: 'House warming',
+        description: 'All come over on wednesday for our housewarming!',
+        startDate,
+        endDate,
+        latitude: 48.8915482,
+        longitude: 2.3170656,
+        streamerDevice: 'abcd',
+        isPrivate: true,
+      };
+
+      const user = await addUserProfile();
+      const jwt = generateJwt(user.insertId);
+
+      const res = await chai.request(server)
+        .post('/api/events')
+        .set({ Authorization: `Bearer ${jwt}` })
+        .send(body);
+
+      res.should.have.status(200);
+      res.body.should.be.a('object');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+    });
+  });
+});
