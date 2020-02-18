d12f1e594c4558cbefaeb1af88cb6dc044b60faa
Adubedat <arthur.dubedat@gmail.com>
2020-02-18 14:38:43 +0100

    [refactor] small change in routes architecture

--- src/server/routes/protectedRoutes/eventsRoutes.js
+++ src/server/routes/protectedRoutes/eventsRoutes.js
@@ -3,16 +3,20 @@ import createEvent from '../../controller/events/createEvent';
 import updateEvent from '../../controller/events/updateEvent';
 import getEvent from '../../controller/events/getEvent';
 import getEventGuests from '../../controller/events/getEventGuests';
+import getEventsByUser from '../../controller/events/getEventsByUser';
+
 
 const router = express.Router();
 
-router.route('/')
+router.route('/events')
   .post(createEvent);
 
-router.route('/:eventId')
+router.get('/me/events', getEventsByUser);
+
+router.route('/events/:eventId')
   .get(getEvent)
   .patch(updateEvent);
 
-router.get('/:eventId/guests', getEventGuests);
+router.get('/events/:eventId/guests', getEventGuests);
 
 export default router;
--- src/server/routes/protectedRoutes/index.js
+++ src/server/routes/protectedRoutes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import eventRoutes from './eventsRoutes';
-import meRoutes from './meRoutes';
+import usersRoutes from './usersRoutes';
 import friendshipsRoutes from './friendshipsRoutes';
 import inviteRoutes from './inviteRoutes';
 import verifyJwt from '../../middlewares/verifyJwt';
@@ -8,9 +8,9 @@ import verifyJwt from '../../middlewares/verifyJwt';
 const router = express.Router();
 
 router.use(verifyJwt);
-router.use('/me', meRoutes);
-router.use('/events', eventRoutes);
-router.use('/invite', inviteRoutes);
+router.use('/', usersRoutes);
+router.use('/', eventRoutes);
+router.use('/', inviteRoutes);
 router.use('/', friendshipsRoutes);
 
 export default router;
