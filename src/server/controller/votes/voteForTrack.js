023c2a4407e7ac6a028650ca00de0bcec46004c1
hadleycode <hadleydaniel@gmail.com>
2020-03-09 13:21:04 +0100

    [fix] Check if the event exists before voting

--- src/server/controller/votes/voteForTrack.js
+++ src/server/controller/votes/voteForTrack.js
@@ -1,10 +1,12 @@
 import { checkUnknownFields } from '../../../helpers/validation';
 import { validateType } from '../events/helpers/validation';
 import Vote from '../../models/voteModel';
+import Event from '../../models/eventModel';
+import { ErrorResponseHandler } from '../../../helpers/error';
 
 export default async function voteForTrack(req, res, next) {
   const { userId } = req.decoded;
-  const { trackId } = req.params;
+  const { eventId, trackId } = req.params;
   const trackIdAsInt = parseInt(trackId, 10);
   const { vote } = req.body;
 
@@ -17,7 +19,14 @@ export default async function voteForTrack(req, res, next) {
 
     // TODO: Check that trackId exists
 
-    // TODO: Check if user is 'going' to the event
+    const event = await Event.getEvent(eventId);
+    if (event[0] == null) {
+      throw new ErrorResponseHandler(404, 'No event found with this ID');
+    }
+
+    // TODO: Check if the user is going to the event
+
+    // TODO: Check if the track is in the event
 
     // TODO: Check if vote is === to 1, -1 or 0
 
