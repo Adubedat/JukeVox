77ab16f63b617b9b9358b05db0b5b4c27b63c969
hadleycode <hadleydaniel@gmail.com>
2020-03-11 16:45:57 +0100

    [wip] Socket structure for joining and leaving event

--- src/server/sockets/listeners.js
+++ src/server/sockets/listeners.js
@@ -1,10 +1,16 @@
+import { joinEvent, leaveEvent } from './socketControllers/eventSocketController';
+
 export default function initListeners(io) {
   io.on('connection', (userSocket) => {
     console.log('User Connected');
 
-    // TODO: add to event room
+    userSocket.on('join_event', (eventId) => {
+      joinEvent(eventId, userSocket, io);
+    });
 
-    // TODO: leave from event room
+    userSocket.on('leave_event', (eventId) => {
+      leaveEvent(eventId, userSocket, io);
+    });
 
     userSocket.on('disconnect', () => {
       console.log('User disconnected');
--- src/server/sockets/socketControllers/eventSocketController.js
+++ src/server/sockets/socketControllers/eventSocketController.js
@@ -1,10 +1,15 @@
-export default function joinEvent(eventId, userSocket, io) {
+export function joinEvent(eventId, userSocket, io) {
   // TODO : If user is valid
   // TODO : If event is valid
   // TODO : If event is ongoing
 
   // TODO: If event does not exist - create it
 
-  // TODO: Add user to event
+  userSocket.join(eventId);
+  console.log(`User just joined event ${eventId}`);
+}
 
+export function leaveEvent(eventId, userSocket, io) {
+  userSocket.leave(eventId);
+  console.log(`User just left the event ${eventId}`);
 }
