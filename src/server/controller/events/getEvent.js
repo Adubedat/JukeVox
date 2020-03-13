31b206aa10c554eea61cd0c96fade87624bcc891
Daniel Hadley <hadleydaniel@gmail.com>
2020-03-13 16:09:10 +0100

    [fix] Check that the user is going to the event before sending details

--- src/server/controller/events/getEvent.js
+++ src/server/controller/events/getEvent.js
@@ -3,12 +3,22 @@ import Event from '../../models/eventModel';
 
 export default async function getEvent(req, res, next) {
   const { eventId } = req.params;
+  const { userId } = req.decoded;
+
   try {
     const event = await Event.getEvent(eventId);
 
     if (event[0] === undefined) {
       throw new ErrorResponseHandler(404, 'No event found with this ID');
     }
+
+    const guestStatusResponse = await Event.getGuestStatusForEvent(userId, eventId);
+    if (guestStatusResponse[0] == null) {
+      throw new ErrorResponseHandler(403, 'Forbidden');
+    }
+
+    // TODO: Get list of tracks associated with event to add to return
+    // TODO: Get list of votes associated with tracks to add to return
 
     res.status(200).send({
       statusCode: 200,
