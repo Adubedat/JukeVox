f10094b3609fa3af1762eef5638676f79f8b4e98
Daniel Hadley <hadleydaniel@gmail.com>
2020-03-17 17:28:32 +0100

    [test] Test getting public events
    Test for 200 success and for 404 no events

--- src/server/controller/events/getPublicEvents.js
+++ src/server/controller/events/getPublicEvents.js
@@ -3,8 +3,6 @@ import Event from '../../models/eventModel';
 
 export default async function getPublicEvents(req, res, next) {
   try {
-    const { userId } = req.decoded;
-    console.log(userId);
     const events = await Event.getPublicEvents();
     if (events[0] === undefined) {
       throw new ErrorResponseHandler(404, 'No public events found');
