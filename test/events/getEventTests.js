55e49c00161533a78ee4ef84decd61ed9ad2e41c
Adubedat <arthur.dubedat@gmail.com>
2020-03-17 12:05:29 +0100

    [test] update test to add location var to events

--- test/events/getEventTests.js
+++ test/events/getEventTests.js
@@ -35,6 +35,7 @@ describe('Events', () => {
       description: 'All come over on wednesday for our housewarming!',
       startDate,
       endDate,
+      location: '46 tests street',
       latitude: 48.8915482,
       longitude: 2.3170656,
       streamerDevice: 'abcd',
@@ -43,10 +44,10 @@ describe('Events', () => {
     };
 
     const eventQuery = 'INSERT INTO Events (CreatorId, Name, Description, \
-        EventPicture, StartDate, EndDate, Latitude, Longitude, \
+        EventPicture, StartDate, EndDate, Location, Latitude, Longitude, \
         StreamerDevice, IsPrivate) VALUES ?;';
     const eventValues = [[creatorId, content.name, content.description, content.eventPicture,
-      content.startDate, content.endDate, content.latitude, content.longitude,
+      content.startDate, content.endDate, content.location, content.latitude, content.longitude,
       content.streamerDevice, content.isPrivate]];
     const event = await sql.query(eventQuery, [eventValues])
       .catch((err) => console.log(err));
@@ -89,7 +90,7 @@ describe('Events', () => {
       res.body.message.should.be.eql(`Event with Id: ${event.insertId}`);
       res.body.data.Name.should.be.eql('House warming');
       res.body.data.should.have.all.keys('CreatorId', 'Name', 'Description', 'EventPicture', 'StartDate',
-        'EndDate', 'Latitude', 'Longitude', 'StreamerDevice', 'IsPrivate', 'Id');
+        'EndDate', 'Location', 'Latitude', 'Longitude', 'StreamerDevice', 'IsPrivate', 'Id');
     });
 
     it('should GET an event (user is invited)', async () => {
@@ -111,7 +112,7 @@ describe('Events', () => {
       res.body.message.should.be.eql(`Event with Id: ${event.insertId}`);
       res.body.data.Name.should.be.eql('House warming');
       res.body.data.should.have.all.keys('CreatorId', 'Name', 'Description', 'EventPicture', 'StartDate',
-        'EndDate', 'Latitude', 'Longitude', 'StreamerDevice', 'IsPrivate', 'Id');
+        'EndDate', 'Location', 'Latitude', 'Longitude', 'StreamerDevice', 'IsPrivate', 'Id');
     });
 
     it('should GET an event (user is not going)', async () => {
@@ -133,7 +134,7 @@ describe('Events', () => {
       res.body.message.should.be.eql(`Event with Id: ${event.insertId}`);
       res.body.data.Name.should.be.eql('House warming');
       res.body.data.should.have.all.keys('CreatorId', 'Name', 'Description', 'EventPicture', 'StartDate',
-        'EndDate', 'Latitude', 'Longitude', 'StreamerDevice', 'IsPrivate', 'Id');
+        'EndDate', 'Location', 'Latitude', 'Longitude', 'StreamerDevice', 'IsPrivate', 'Id');
     });
 
     it('should not GET an event with unknown id', async () => {
--- test/events/inviteGuestToEventTests.js
+++ test/events/inviteGuestToEventTests.js
@@ -35,6 +35,7 @@ describe('Invite', () => {
       description: 'All come over on wednesday for our housewarming!',
       startDate,
       endDate,
+      location: '46 tests street',
       latitude: 48.8915482,
       longitude: 2.3170656,
       streamerDevice: 'abcd',
@@ -43,10 +44,10 @@ describe('Invite', () => {
     };
 
     const eventQuery = 'INSERT INTO Events (CreatorId, Name, Description, \
-        EventPicture, StartDate, EndDate, Latitude, Longitude, \
+        EventPicture, StartDate, EndDate, Location, Latitude, Longitude, \
         StreamerDevice, IsPrivate) VALUES ?;';
     const eventValues = [[creatorId, content.name, content.description, content.eventPicture,
-      content.startDate, content.endDate, content.latitude, content.longitude,
+      content.startDate, content.endDate, content.location, content.latitude, content.longitude,
       content.streamerDevice, content.isPrivate]];
     const event = await sql.query(eventQuery, [eventValues])
       .catch((err) => console.log(err));
