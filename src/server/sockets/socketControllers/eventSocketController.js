ad1782fba636c42f59ba649599ab81f4f2af56e1
Daniel Hadley <hadleydaniel@gmail.com>
2020-03-19 18:02:05 +0100

    [wip] Emit exceptions if error

--- src/server/sockets/socketControllers/eventSocketController.js
+++ src/server/sockets/socketControllers/eventSocketController.js
@@ -3,28 +3,31 @@ import Event from '../../models/eventModel';
 import DATETIME_FORMAT from '../../constants';
 
 export async function joinEvent(userId, eventId, socket, io) {
-  const event = await Event.getEvent(eventId);
+  try {
+    const event = await Event.getEvent(eventId);
+    if (event[0] === undefined) {
+      socket.emit('exception', { code: 404, message: 'Event not found' });
+      return;
+    }
 
-  if (event[0] === undefined) {
-    // TODO: Throw error
-    console.log('No event found with this ID');
-    return;
+    const guestStatusResponse = await Event.getGuestStatusForEvent(userId, eventId);
+    if (guestStatusResponse[0] == null || guestStatusResponse[0].GuestStatus !== 'Going') {
+      socket.emit('exception', { code: 403, message: 'Forbidden. User not going' });
+      return;
+    }
+
+    const timeNow = moment().format(DATETIME_FORMAT);
+    if (moment(timeNow).isBefore(event[0].StartDate) || moment(timeNow).isAfter(event[0].EndDate)) {
+      socket.emit('exception', { code: 403, message: 'Forbidden. Event not ongoing' });
+      return;
+    }
+
+    socket.join(eventId);
+    socket.emit('success', { code: 200, message: 'Successfully joined event' });
+  } catch (err) {
+    console.log(err);
+    socket.emit('exception', { code: 500, message: 'Internal Server Error' });
   }
-
-  const guestStatusResponse = await Event.getGuestStatusForEvent(userId, eventId);
-  if (guestStatusResponse[0] == null || guestStatusResponse[0].GuestStatus !== 'Going') {
-    // TODO: Throw error
-    console.log('Can only connect if going to event');
-  }
-
-  const timeNow = moment().format(DATETIME_FORMAT);
-  if (moment(timeNow).isBefore(event[0].StartDate) || moment(timeNow).isAfter(event[0].EndDate)) {
-    // TODO: Throw error
-    console.log('Forbidden. Event is not ongoing');
-  }
-
-  socket.join(eventId);
-  console.log(`User ${userId} just joined event ${eventId}`);
 }
 
 export function leaveEvent(userId, eventId, socket, io) {
