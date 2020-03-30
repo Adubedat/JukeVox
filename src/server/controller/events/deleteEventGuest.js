b868db23a5ac63cd6f054909176564adfe2072ef
Daniel Hadley <hadleydaniel@gmail.com>
2020-03-30 11:51:24 +0200

    [feat] Add route to delete guest from event
    Can be used for:
    - removing an invitation
    - kicking a guest from an event
    - removing a public event if misclicked

---
+++ src/server/controller/events/deleteEventGuest.js
@@ -0,0 +1,39 @@
+import { ErrorResponseHandler } from '../../../helpers/error';
+import Event from '../../models/eventModel';
+import { checkUnknownFields } from '../../../helpers/validation';
+
+export default async function deleteEventGuest(req, res, next) {
+  const { userId } = req.decoded;
+  const { eventId, guestId } = req.params;
+
+  try {
+    checkUnknownFields(['eventId', 'guestId'], req.params);
+
+    const event = await Event.getEvent(eventId);
+    if (event[0] === undefined) {
+      throw new ErrorResponseHandler(404, 'Event not found');
+    }
+
+    if (userId !== Event.CreatorId) {
+      throw new ErrorResponseHandler(403, 'Forbidden');
+    }
+
+    const guest = await Event.getGuestStatusForEvent(guestId, eventId);
+    if (guest[0] === undefined) {
+      throw new ErrorResponseHandler(403, 'Forbidden');
+    }
+
+    const response = Event.deleteEventGuest(eventId, guestId);
+
+    if (response.affectedRows !== 1) {
+      throw new ErrorResponseHandler(500, 'Internal server error');
+    }
+
+    res.status(200).send({
+      message: 'Guest deleted',
+      statusCode: 200,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
