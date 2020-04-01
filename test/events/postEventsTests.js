c4964e95c292bdd43ca55791d2ccb43cce3d8a88
Daniel Hadley <hadleydaniel@gmail.com>
2020-04-01 14:40:10 +0200

    [test] Change the tests to reflect new RestrictVote

--- test/events/postEventsTests.js
+++ test/events/postEventsTests.js
@@ -385,6 +385,7 @@ describe('Events', () => {
         streamerDevice: 'abcd',
         isPrivate: true,
         eventPicture: 'defaultPicture1',
+        restrictVotingToEventHours: true,
       };
 
       const user = await addUserProfile();
