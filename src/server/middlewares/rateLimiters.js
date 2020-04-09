ce5c4a5d37f4874e2a1c345808aa9f80d6e409a7
Daniel Hadley <hadleydaniel@gmail.com>
2020-04-09 16:34:34 +0200

    [fix] Set rate limiter depending on test or prod env

--- src/server/middlewares/rateLimiters.js
+++ src/server/middlewares/rateLimiters.js
@@ -1,15 +1,26 @@
 import rateLimit from 'express-rate-limit';
 
+let rateLimitOpenRoutes = 1;
+let rateLimitProtectedRoutes = 1;
+
+if (process.env.NODE_ENV === 'production') {
+  rateLimitOpenRoutes = 30;
+  rateLimitProtectedRoutes = 60;
+} else if (process.env.NODE_ENV === 'test') {
+  rateLimitOpenRoutes = 3000;
+  rateLimitProtectedRoutes = 6000;
+}
+
 export const openRoutesRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
-  max: 30,
+  max: rateLimitOpenRoutes,
   message: 'You have exceeded the 30 requests in 15 minutes limit!',
   headers: true,
 });
 
 export const loggedInRateLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute in miliseconds
-  max: 60,
+  max: rateLimitProtectedRoutes,
   message: 'You have exceeded the 60 requests in 1 minute',
   headers: true,
 });
