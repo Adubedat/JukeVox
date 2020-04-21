2faebc94f74f3798f7d3bdff1e7a7c56ccf956eb
Daniel Hadley <hadleydaniel@gmail.com>
2020-04-21 11:04:02 +0200

    [feat] Add socket for remote control
    The server will now emit and receive actions to
    do with the status play/pause of the music allowing
    for remote control to happen

--- src/server/sockets/socketControllers/eventSocketController.js
+++ src/server/sockets/socketControllers/eventSocketController.js
@@ -1,6 +1,4 @@
-import moment from 'moment';
 import Event from '../../models/eventModel';
-import DATETIME_FORMAT from '../../constants';
 
 export async function joinEvent(userId, eventId, socket, io) {
   try {
@@ -23,6 +21,29 @@ export async function joinEvent(userId, eventId, socket, io) {
     socket.join(eventId);
     socket.emit('success', {
       code: 200, message: 'Successfully joined event', event: 'join_event', eventId,
+    });
+  } catch (err) {
+    console.log(err);
+    socket.emit('exception', {
+      code: 500, message: 'Internal Server Error', event: 'join_event', eventId,
+    });
+  }
+}
+
+export async function changeStatusOfMusic(userId, eventId, status, socket, io) {
+  try {
+    const playerController = await Event.getPlayerControllerStatus(eventId, userId);
+    if (playerController[0] == null || playerController[0].HasPlayerControl !== 1) {
+      socket.emit('exception', {
+        code: 403, message: 'Forbidden. User not remote controller', event: 'remote_controller', eventId,
+      });
+      return;
+    }
+
+    io.to(eventId).emit('music_status_change', { data: { userId, eventId, status } });
+
+    socket.emit('success', {
+      code: 200, message: 'Successfully changed control', event: 'remote_controller', eventId,
     });
   } catch (err) {
     console.log(err);
