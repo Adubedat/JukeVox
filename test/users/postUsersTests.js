e192ed6bac96aa3e45b705193c8a215b569a7dd6
Daniel Hadley <hadleydaniel@gmail.com>
2020-04-23 12:00:35 +0200

    [test][refactor] Refactor post Users tests into async funcitons

--- test/users/postUsersTests.js
+++ test/users/postUsersTests.js
@@ -27,122 +27,113 @@ describe('Users', () => {
 
   // TODO: In all of the 'should NOT POST' check that no user was actually added
   describe('/POST users', () => {
-    it('should not POST a user without a password field', (done) => {
+    it('should not POST a user without a password field', async () => {
       const user = {
         username: 'Daniel',
         email: 'daniel@mail.com',
       };
-      chai.request(server)
+      const res = await chai.request(server)
         .post('/users')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('status').eql('error');
-          res.body.should.have.property('statusCode');
-          res.body.should.have.property('message');
-          res.body.message.should.eql('TypeError password: expected string but received undefined');
-          done();
-        });
+        .send(user);
+
+      res.should.have.status(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('TypeError password: expected string but received undefined');
     });
 
     // TODO: Write test for username too long
-    it('should not POST a user without a username field', (done) => {
+    it('should not POST a user without a username field', async () => {
       const user = {
         email: 'daniel@mail.com',
         password: 'abcdefghijk',
       };
-      chai.request(server)
+      const res = await chai.request(server)
         .post('/users')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('status').eql('error');
-          res.body.should.have.property('statusCode');
-          res.body.should.have.property('message');
-          res.body.message.should.eql('TypeError username: expected string but received undefined');
-          done();
-        });
+        .send(user);
+
+      res.should.have.status(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('TypeError username: expected string but received undefined');
     });
 
-    it('should not POST a user without an email field', (done) => {
+    it('should not POST a user without an email field', async () => {
       const user = {
         username: 'Daniel',
         password: 'abcdefghijk',
       };
-      chai.request(server)
+
+      const res = await chai.request(server)
         .post('/users')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('status').eql('error');
-          res.body.should.have.property('statusCode');
-          res.body.should.have.property('message');
-          res.body.message.should.eql('TypeError email: expected string but received undefined');
-          done();
-        });
+        .send(user);
+
+      res.should.have.status(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('TypeError email: expected string but received undefined');
     });
 
-    it('should not POST a user with an invalid username field', (done) => {
+    it('should not POST a user with an invalid username field', async () => {
       const user = {
         username: 'Daniel&Daniel',
         password: 'abcdefghijk',
         email: 'daniel@mail.com',
       };
-      chai.request(server)
+
+      const res = await chai.request(server)
         .post('/users')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('status').eql('error');
-          res.body.should.have.property('statusCode');
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Username must only contain numbers or letters');
-          done();
-        });
+        .send(user);
+
+      res.should.have.status(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('Username must only contain numbers or letters');
     });
 
-    it('should not POST a user with an invalid password field', (done) => {
+    it('should not POST a user with an invalid password field', async () => {
       const user = {
         username: 'Daniel',
         password: 'abc',
         email: 'daniel@mail.com',
       };
-      chai.request(server)
+
+      const res = await chai.request(server)
         .post('/users')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('status').eql('error');
-          res.body.should.have.property('statusCode');
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Your password must have at least 10 characters');
-          done();
-        });
+        .send(user);
+
+      res.should.have.status(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('Your password must have at least 10 characters');
     });
 
-    it('should not POST a user with an invalid email field', (done) => {
+    it('should not POST a user with an invalid email field', async () => {
       const user = {
         username: 'Daniel',
         password: 'abcdefghijk',
         email: 'daniel',
       };
-      chai.request(server)
+      const res = await chai.request(server)
         .post('/users')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(400);
-          res.body.should.be.a('object');
-          res.body.should.have.property('status').eql('error');
-          res.body.should.have.property('statusCode');
-          res.body.should.have.property('message');
-          res.body.message.should.eql('Email not correctly formatted');
-          done();
-        });
+        .send(user);
+
+      res.should.have.status(400);
+      res.body.should.be.a('object');
+      res.body.should.have.property('status').eql('error');
+      res.body.should.have.property('statusCode');
+      res.body.should.have.property('message');
+      res.body.message.should.eql('Email not correctly formatted');
     });
 
     it('should not POST a user with an existing username', async () => {
@@ -188,21 +179,20 @@ describe('Users', () => {
     });
 
     // TODO: Actually check that a user was added to the DB and UserAccount was made
-    it('should POST a user', (done) => {
+    it('should POST a user', async () => {
       const user = {
         username: 'Daniel',
         email: 'arthur.dubedat@gmail.com',
         password: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
       };
-      chai.request(server)
+
+      const res = await chai.request(server)
         .post('/users')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.have.property('message').eql('User created. Please check your mail!');
-          done();
-        });
+        .send(user);
+
+      res.should.have.status(201);
+      res.body.should.be.a('object');
+      res.body.should.have.property('message').eql('User created. Please check your mail!');
     });
   });
 });
